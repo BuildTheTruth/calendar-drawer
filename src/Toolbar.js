88596195e821ff6824bf6395f29d4b68c758810c
denis-dev <denis.dev@kakaoenterprise.com>
2022-06-28 17:09:45 +0900

    remove google font

--- src/Toolbar.js
+++ src/Toolbar.js
@@ -1,18 +1,26 @@
+// import ArrowLeftIcon from './ArrowLeftIcon'
+import { ReactComponent as ChevronLeft } from './svg/chevron-left-solid.svg'
+import { ReactComponent as CHevronRight } from './svg/chevron-right-solid.svg'
+
 export default function Toolbar({ date, onNavigate }) {
-  const month = ("0" + (date.getMonth() + 1)).slice(-2);
-  const label = `${date.getFullYear()}. ${month}`;
+  const month = ('0' + (date.getMonth() + 1)).slice(-2)
+  const label = `${date.getFullYear()}. ${month}`
 
   return (
     <div className="rbc-toolbar">
       <span className="rbc-btn-group">
-        <button type="button" onClick={() => onNavigate("PREV")}>
-          <span className="material-symbols-outlined">chevron_left</span>
+        <button type="button" onClick={() => onNavigate('PREV')}>
+          <span className="svg-wrapper">
+            <ChevronLeft width="16" height="16" />
+          </span>
         </button>
         <span className="rbc-toolbar-label">{label}</span>
-        <button type="button" onClick={() => onNavigate("NEXT")}>
-          <span className="material-symbols-outlined">chevron_right</span>
+        <button type="button" onClick={() => onNavigate('NEXT')}>
+          <span className="svg-wrapper">
+            <CHevronRight width="16" height="16" />
+          </span>
         </button>
       </span>
     </div>
-  );
+  )
 }
